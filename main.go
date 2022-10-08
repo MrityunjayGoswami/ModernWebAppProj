92f9f4e69f871fd8e19d0a2ebc61044f09356e3e
MrityunjayGoswami <mrityunjaygoswami96@gmail.com>
2022-10-08 18:10:58 +0530

    new

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+)
+
+type Person struct{
+	FirstNmae string `json:"first_name"`
+	LastName string `json:"last_name"`
+}
 
 func main(){
-	fmt.Println("Hello")
+	myJson:= `
+[
+	{
+		"first_name": "Clark",
+		"last_name": "Kent"
+	},
+	{
+		"first_name": "Bruce",
+		"last_name": "Wayne"
+	}
+]`
+	var unmarshalled []Person
+	err := json.Unmarshal([]byte(myJson),&unmarshalled)
+	if err != nil{
+		log.Println("Erroe unmarshling Json",err)
+	}
+	log.Printf("Unmarshalled: %v", unmarshalled)
 }
